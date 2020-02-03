6c3bf6808da1e8482de11fdd9a41801e0beff3ed
Autom-liu <349576362@163.com>
2020-02-04 03:51:28 +0800

    日期格式工具类修改

--- zwebapp-common/src/main/java/com/scnu/zwebapp/common/util/DateUtils.java
+++ zwebapp-common/src/main/java/com/scnu/zwebapp/common/util/DateUtils.java
@@ -3,28 +3,25 @@ package com.scnu.zwebapp.common.util;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
 	
 	private DateUtils() {}
 	
-	private static final ThreadLocal<DateFormat> standarDateFormat = new ThreadLocal<DateFormat>() {
-		
-		@Override
-		protected DateFormat initialValue() {
-			return new SimpleDateFormat("yyyy-MM-dd");
+	private static final Map<DatePattern, ThreadLocal<DateFormat>> STANDAR_DATEFORMATTER_MAP = new HashMap<>();
+	
+	static {
+		DatePattern[] patterns = DatePattern.values();
+		for(DatePattern pattern : patterns) {
+			STANDAR_DATEFORMATTER_MAP.put(pattern, pattern.getFormatter());
 		}
-		
-	};
-	
-	
-	public static String toString(Date date) {
-		DateFormat formatter = standarDateFormat.get();
-		return formatter.format(date);
 	}
 	
 	public static String toString(Date date, DatePattern pattern) {
-		DateFormat formatter = standarDateFormat.get();
+		ThreadLocal<DateFormat> formaterLocal = STANDAR_DATEFORMATTER_MAP.get(pattern);
+		DateFormat formatter = formaterLocal.get();
 		return formatter.format(date);
 	}
 	
@@ -35,12 +32,27 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
 		SIMPLE_DATE_FORMATER,
 		/** yyyy-mm-dd hh:mi:ss **/
 		STANDAR_DATETIME_FORAMTER,
-		/**yyyymmdd hhmiss **/
+		/**yyyymmddhhmiss **/
 		SIMPLE_DATETIME_FORMATER,
+		/**yyyymmddhhmissSSS**/
+		SIMPLE_DATESEC_FORMATER,
 		/** hh:mi:ss **/
 		STANDAR_TIME_FORMATER,
 		/** hhmiss **/
-		SIMPLE_TIME_FORMATER,
+		SIMPLE_TIME_FORMATER,;
+		
+		private ThreadLocal<DateFormat> getFormatter() {
+			switch(this) {
+			case STANDAR_DATE_FORMATER: return ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd"));
+			case SIMPLE_DATE_FORMATER: return ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyyMMdd"));
+			case STANDAR_DATETIME_FORAMTER: return ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd hh:mm:ss"));
+			case SIMPLE_DATETIME_FORMATER: return ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyyMMddhhmmss"));
+			case SIMPLE_DATESEC_FORMATER: return ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyyMMddhhmmssSSS"));
+			case STANDAR_TIME_FORMATER: return ThreadLocal.withInitial(() -> new SimpleDateFormat("hh:mm:ss"));
+			case SIMPLE_TIME_FORMATER: return ThreadLocal.withInitial(() -> new SimpleDateFormat("hhmmss"));
+			default: return null;
+			}
+		}
 	}
 
 }
